e20d8768e2cc2f557b6c3a4ae35f83bdbec2cf34
Pule <proceedingmc@gmail.com>
2023-08-11 21:07:29 +0200

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
-
+    # Get the number of arguments passed (excluding the script name)
     count = len(sys.argv) - 1
 
     if count == 0:
@@ -12,5 +12,8 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
+    # Iterate over the arguments and print each argument with it's position
     for i in range(count):
+        # i + 1 is the position of the argument (starting at 1)
+        # sys.argv[i + 1] gets the actual value
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
