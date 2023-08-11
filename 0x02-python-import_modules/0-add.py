0a53dc28402ccef781721be29f12babbd705e1c5
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-08-11 19:30:28 +0200

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 # Importing the function from add_0.py
-add_module = __import__("add_0")
+#add_module = __import__("add_0")
 
-# Assigning values to variables
-a = 1
-b = 2
+if __name__ == "__main__":
+  """sum of 1 and 2"""
+  from add_0 import add
+  """Assigning values to variables"""
+  a = 1
+  b = 2
 
-# Calling the add function and printing the result
-print(f"{a} + {b} = {add_module.add(a, b)}")
+  # Calling the add function and printing the result
+  print("{} + {} = {}".format(a, b, add(a, b)))
