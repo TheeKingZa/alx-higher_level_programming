e9d39114347865619f3a57cab69637cafdb52fe1
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-08-11 19:25:04 +0200

    0-add.py
    Edited for interactive mode

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # Importing the function from add_0.py
-from add_0 import add
+add_module = __import__("add_0")
 
 # Assigning values to variables
 a = 1
 b = 2
 
 # Calling the add function and printing the result
-print(f"{a} + {b} = {add(a, b)}")
+print(f"{a} + {b} = {add_module.add(a, b)}")
