2632801903c623093a0bc41adbca9b369fde9e5c
Pule <proceefingmc@gmail.com>
2023-08-11 22:36:34 +0200

    4-print_float.py

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+# Define the number with a float value
 number = 3.14159
-formatted_float = "{:.2f}".format(number)
+
+# Format the float using f-string with 2 decimal places
+formatted_float = f"{number:.2f}"
+
+# Print the formatted float with the specified precision
 print(f"Float:{formatted_float}\n")
