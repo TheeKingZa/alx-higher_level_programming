f4ec89784822ce6e093f78f0a69c6ca02b661b45
Pule <proceefingmc@gmail.com>
2023-08-11 21:44:04 +0200

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,13 @@ if __name__ == "__main__":
     """Print the addition of all arguments."""
     import sys
 
+    # Initialize a variable to store the sum of arguments
     total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+
+    # Loop through command-line arguments (excluding script name)
+    for i in range(1, len(sys.argv)):
+        # Convert each argument to an integer and add it to the total
+        total += int(sys.argv[i])
+
+    # Print the total sum of arguments
+    print(total)
