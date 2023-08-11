13f6b6f172393920ea4d99cb902d37f5194ff6fd
Pule <proceedingmc@gmail.com>
2023-08-11 19:47:14 +0200

    add_0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+# Importing the function from add_0.py
+# add_module = __import__("add_0")
 
 if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
+    """sum of 1 and 2"""
     from add_0 import add
-
+    """Assigning values to variables"""
     a = 1
     b = 2
+
+    # Calling the add function and printing the result
     print("{} + {} = {}".format(a, b, add(a, b)))
