b21ef9302d2cbc4f8c3cde5c68b0785dad6e1c94
Pule <proceedingmc@gmail.com>
2023-08-14 20:44:39 +0200

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,14 +3,15 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     # Create an empty tuple to store the result
     new_tuple = ()
-    
-    # Pad both input tuples with zeros to ensure they each have at least two elements
+
+    # Pad both input
+    # tuples with zeros to ensure
+    # each have at least two elements
     tuple_1 = tuple_a + (0, 0)
     tuple_2 = tuple_b + (0, 0)
-    
+
     # Calculate the sum of the corresponding elements and create the new tuple
     new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
-    
+
     # Return the new tuple
     return new_tuple
-
