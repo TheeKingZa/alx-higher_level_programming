35319d6be7f4c04f0e94931849237fdb0432fe94
Pule <proceedingmc@gmail.com>
2023-08-14 12:02:42 +0200

    ptint last int

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
+# Prototype
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
+        '''list the range of length at index i'''
         print("{:d}".format(my_list[i]))
