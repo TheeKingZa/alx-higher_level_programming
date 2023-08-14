1a33f2eac44bf11525ad016a56c3b0a7d1da2604
Pule <proceedingmc@gmail.com>
2023-08-14 21:23:55 +0200

    11-delete_at.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,14 +3,16 @@
 def multiple_returns(sentence):
     # Create an empty tuple to store the result
     my_tuple = ()
-    
+
     # Check if the sentence is empty
     if len(sentence) == 0:
         # If the sentence is empty, create a tuple with 0 and "None"
         my_tuple = 0, "None"
     else:
-        # If the sentence is not empty, create a tuple with the length and the first character
+        # If the sentence is not empty
+        # create a tuple with the length
+        # ..and the first character
         my_tuple = len(sentence), sentence[0]
-    
+
     # Return the created tuple
     return my_tuple
