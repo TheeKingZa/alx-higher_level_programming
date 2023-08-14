271e5bdba60164ff325c43ad66b7185c699fb4c5
Pule <proceedingmc@gmail.com>
2023-08-14 21:35:56 +0200

    102-magic_calclulation.py

--- 0x02-python-import_modules/102-magic_calclulation.py
+++ 0x02-python-import_modules/102-magic_calclulation.py
@@ -1,12 +1,12 @@
 def magic_calculation(a, b):
     # Import functions 'add' and 'sub' from the module 'magic_calculation_102'
     from magic_calculation_102 import add, sub
-    
+
     # Compare a and b
     if a < b:
         # Call the 'add' function and store the result in 'c'
         c = add(a, b)
-        
+
         # Iterate through a range of values [4, 5]
         for i in range(4, 6):
             # Call the 'add' function with arguments 'c' and 'i', update 'c'
@@ -14,6 +14,6 @@ def magic_calculation(a, b):
     else:
         # Call the 'sub' function and return the result
         return sub(a, b)
-    
+
     # Return the final value of 'c'
     return c
