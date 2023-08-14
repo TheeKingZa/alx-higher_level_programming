d25c0a68afd5cf27529d4a0fc6b679460c7345ff
Pule <proceedingmc@gmail.com>
2023-08-14 16:51:39 +0200

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,12 +3,7 @@
 def print_reversed_list_integer(my_list=[])
 
 
-# Check if the input list is not empty
 if my_list:
-    # Reverse the elements of the list in-place
     my_list.reverse()
-
-    # Iterate over the indices of the reversed list
     for i in range(len(my_list)):
-        # Print the current element in the reversed list
         print("{:d}".format(my_list[i]))
