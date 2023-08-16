914976570d6efc15bc57f4ac06890925a500d2a0
Pule <proceedingmc@gmail.com>
2023-08-16 20:43:35 +0200

    5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -2,7 +2,7 @@
 def number_keys(a_dictionary):
     '''
     A function that returns the number of keys in a dictionary.
-    
+
     :param a_dictionary: The input dictionary.
     :return: The number of keys in the dictionary.
     '''
@@ -13,6 +13,6 @@ def number_keys(a_dictionary):
         # Iterate through the keys in the dictionary
         count += 1
         # Increment the counter for each key
-        
+
     return count
     # Return the total number of keys
