0f00d8c6ea30bca11a47c2bfd28a37cd6bc9b319
Pule <proceedingmc@gmail.com>
2023-08-17 01:00:19 +0200

    11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -2,8 +2,9 @@
 
 def multiply_list_map(my_list=[], number=0):
     '''
-    A function that multiplies each element of a list by a specified number using map.
-    
+    A function that multiplies each element of a
+    list by a specified number using map.
+
     :param my_list: The input list (default is an empty list).
     :param number: The number to multiply each element by (default is 0).
     :return: A new list with elements multiplied by the specified number.
