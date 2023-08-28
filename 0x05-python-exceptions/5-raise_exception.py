3da535770416918493adf4478aba57ee7417d34d
Pule <proceedingmc@gmail.com>
2023-08-28 19:16:33 +0200

    raise exception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    """
+    Raises a TypeError exception.
+
+    Raises:
+        TypeError: Always raised when this function is called.
+    """
+    raise TypeError
