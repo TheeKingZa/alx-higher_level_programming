1344420fb042590e5c896abd255e25414a1eafe5
Pule <proceedingmc@gmail.com>
2023-08-29 16:00:32 +0200

    Empty Class that defines Square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains the definition of the empty class Square.
+"""
+
+
+class Square:
+    """
+    This is the Square class.
+    It defines a square.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the Square class.
+        This is an empty constructor for now.
+        """
+        pass
