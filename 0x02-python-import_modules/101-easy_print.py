5449244c9f7c7e33e1ac3674b5617707b1903cd7
Pule <proceedingmc@gmail.com>
2023-08-29 20:34:52 +0200

    101-easy_print.py

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
-# Importing sys module to access stdout
-import sys
+import sys [sys.stdout.write("#pythoniscool\n") for i in range(1)]
 
 # Using a list comprehension to write the desired output to stdout
 # We iterate over a range with just one element (1), so the loop runs once
 # Within the loop, we use sys.stdout.write to write the string to stdout
-[sys.stdout.write("#pythoniscool\n") for i in range(1)]
