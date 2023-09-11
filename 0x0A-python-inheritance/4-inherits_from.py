3e720cf353264e2190b1b1a234fba6409f07c269
Pule <proceedingmc@gmail.com>
2023-09-11 22:05:21 +0200

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""inherits_from module - defines a function to check class inheritance"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an instance of a class inherited from a specified class"""
+    return isinstance(obj, a_class) and type(obj) is not a_class
