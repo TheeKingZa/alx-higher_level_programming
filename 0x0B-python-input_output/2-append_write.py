385885da8a2a6d3e98fa651a00f67602b20ba452
Pule <proceedingmc@gmail.com>
2023-09-12 19:15:38 +0200

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""append_write module - defines a function to append a string to a text file"""
+"""
+append_write module
+defines a function to append a string to a text file
+"""
 
 
 def append_write(filename="", text=""):
-    """Appends a string to the end of a text file (UTF8) and returns the number of characters added."""
+    """Appends a string to the end of a text file (UTF8)
+    Returns the number of characters added."""
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
-
