0075165563ec188c774348ca2322715f3d76f7d5
Pule <proceedingmc@gmail.com>
2023-09-12 20:09:39 +0200

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Student module - defines a Student class"""
+
+
+class Student:
+    """Student class to represent a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance with
+        @ first_name
+        @ last_name
+        @ age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dictionary representation of
+        A Student instance with specified attributes."""
+        if attrs is None:
+            return self.__dict__
+        return {key: value for key, value in self.__dict__.items()
+                if key in attrs}
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance from
+        A given JSON dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
