b803abd56be94ba85b43bc75bc5bea067180c26e
Pule <proceedingmc@gmail.com>
2023-09-12 10:37:44 +0200

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Student module - defines a Student class"""
+
+
+class Student:
+    """Student class to represent a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance with
+        @first_name
+        @last_name
+        @age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns a dictionary representation of a Student instance."""
+        return self.__dict__
