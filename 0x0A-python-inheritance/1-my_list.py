af0c59346bfaa2884a24567fbe94024cdea43208
Pule <proceedingmc@gmail.com>
2023-09-13 21:01:59 +0200

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
+"""
+contains the MyList class
+"""
+
+
+# Define the MyList class as a subclass of the built-in list class.
 class MyList(list):
+    """a subclass of list"""
+
+    # Initialize the MyList object.
+    def __init__(self):
+        """initializes the object"""
+        # Call the constructor of the base class (list)
+        # to initialize the object.
+        super().__init__()
+
+    # Define a method to print the sorted list.
     def print_sorted(self):
-        """
-        Public instance method to print the list in
-        Ascending order.
-        """
-        sorted_list = sorted(self)
-        print(sorted_list)
+        """prints the sorted list"""
+        # Use the sorted function
+        # to create a sorted copy
+        # of the list and print it.
+        print(sorted(self))
