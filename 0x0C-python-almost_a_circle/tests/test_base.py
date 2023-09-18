f4758f51378f269ee12b04f51a60b54d4d9abafe
Pule <proceedingmc@gmail.com>
2023-09-18 11:05:39 +0200

    tests/

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,34 +1,70 @@
+#!/usr/bin/python3
+"""Unit tests for Base class."""
 import unittest
-from models.rectangle import Rectangle
+import json
 from models.base import Base
 
+
 class TestBase(unittest.TestCase):
+    """Test cases for the Base class."""
 
     def test_instance_creation(self):
+        """Test creating a Base instance with and without an ID."""
         b1 = Base()
-        b2 = Base()
         self.assertEqual(b1.id, 1)
+
+        b2 = Base()
         self.assertEqual(b2.id, 2)
 
-    def test_instance_with_custom_id(self):
-        b = Base(10)
-        self.assertEqual(b.id, 10)
+        b3 = Base(100)
+        self.assertEqual(b3.id, 100)
 
     def test_to_json_string(self):
-        r1 = Rectangle(1, 2)
-        json_string = Base.to_json_string([r1.to_dictionary()])
-        self.assertEqual(json_string, '[{"id": 1, "width": 1, "height": 2, "x": 0, "y": 0}]')
+        """Test the to_json_string method of Base class."""
+        b1 = Base(1)
+        b2 = Base(2)
+        b3 = Base(3)
+
+        dict_list = [b1.to_dictionary(), b2.to_dictionary(), b3.to_dictionary()]
+        json_string = Base.to_json_string(dict_list)
+
+        expected_json_string = json.dumps([{
+            "id": 1,
+            "width": 1,
+            "height": 1,
+            "x": 0,
+            "y": 0
+        }, {
+            "id": 2,
+            "width": 1,
+            "height": 1,
+            "x": 0,
+            "y": 0
+        }, {
+            "id": 3,
+            "width": 1,
+            "height": 1,
+            "x": 0,
+            "y": 0
+        }])
+
+        self.assertEqual(json_string, expected_json_string)
 
     def test_from_json_string(self):
-        """Test from_json_string method."""
-        json_string = '[{"id": 1, "width": 2, "height": 3, "x": 4, "y": 5}]'
-        list_dicts = Base.from_json_string(json_string)
-        instances = [Rectangle.create(**d) for d in list_dicts]
-        self.assertEqual(instances[0].id, 1)
-        self.assertEqual(instances[0].width, 2)
-        self.assertEqual(instances[0].height, 3)
-        self.assertEqual(instances[0].x, 4)
-        self.assertEqual(instances[0].y, 5)
+        """Test the from_json_string method of Base class."""
+        json_string = '[{"id": 1, "width": 1, "height": 2, "x": 0, "y": 0}]'
+        dict_list = Base.from_json_string(json_string)
+
+        expected_dict_list = [{
+            "id": 1,
+            "width": 1,
+            "height": 2,
+            "x": 0,
+            "y": 0
+        }]
+
+        self.assertEqual(dict_list, expected_dict_list)
+
 
 if __name__ == '__main__':
     unittest.main()
