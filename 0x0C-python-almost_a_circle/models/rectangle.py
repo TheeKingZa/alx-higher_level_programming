e4266e20bfacdf4c6f0228711441b7c449d6431b
Pule <proceedingmc@gmail.com>
2023-09-19 11:31:39 +0200

    rectangle update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -133,9 +133,9 @@ class Rectangle(Base):
                 self.x = args[3]
             if len(args) >= 5:
                 self.y = args[4]
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+#        else:
+#           for key, value in kwargs.items():
+#               setattr(self, key, value)
 
     def to_dictionary(self):
         """Return
