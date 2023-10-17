3f2255959dfc9712cda11e742def708e4a836b03
Pule <proceedingmc@gmail.com>
2023-10-17 18:42:00 +0200

    Sql

--- 0x0D-SQL_introduction/0-list_database.sql
+++ 0x0D-SQL_introduction/0-list_database.sql
@@ -1,7 +1,2 @@
-You have unmerged paths.
-  (fix conflicts and run "git commit")
-  (use "git merge --abort" to abort the merge)
-
-Unmerged paths:
-  (use "git add <file>..." to mark resolution)
-        both modified:   0-list_databases.sql
+-- Lists all databases of my MySQL server.
+SHOW DATABASES;
