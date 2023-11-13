5fce1cf4217ee9fc5dc62e77686d906c9697bc7d
Pule <proceedingmc@gmail.com>
2023-11-13 12:40:03 +0200

    4-concat.js

---
+++ 0x12-javascript_warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// The above line is a shebang indicating that the script should be run using the specified interpreter, in this case, 'node'.
+
+// Print a concatenated string using command-line arguments.
+console.log(process.argv[2] + ' is ' + process.argv[3]);
