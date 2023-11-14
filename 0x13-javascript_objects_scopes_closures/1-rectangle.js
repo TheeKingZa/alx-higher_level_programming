6857292c891b07438c91714a356b54aba88d5593
Pule <proceedingmc@gmail.com>
2023-11-14 18:28:53 +0200

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// The above line is a shebang indicating that the script should be run using the specified interpreter, in this case, 'node'.
+
+// Define a class named 'Rectangle'.
+class Rectangle {
+  // Constructor method to initialize 'width' and 'height'.
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+// Export the 'Rectangle' class as part of the module.
+module.exports = Rectangle;
