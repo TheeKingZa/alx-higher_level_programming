0fd4cf61d7126a00a532599a89cac31b3b71976e
Pule <proceedingmc@gmail.com>
2023-11-14 18:26:49 +0200

    0-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// The above line is a shebang indicating that the script should be run using the specified interpreter, in this case, 'node'.
+
+// Define a class named 'Rectangle'.
+class Rectangle {
+}
+
+// Export the 'Rectangle' class as part of the module.
+module.exports = Rectangle; 
