d44822c48a129c32744d3c7ad92c93e0fb2abe91
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-11-14 09:51:13 +0200

    Delete 0x12-javascript_warm_up directory

--- 0x12-javascript_warm_up/101-call_me_moby.js
+++
@@ -1,9 +0,0 @@
-const myModule = require('./path-to-your-script'); // Replace with the actual path to your script
-
-// Define a function to be called by callMeMoby
-function printHello() {
-  console.log('Hello!');
-}
-
-// Use callMeMoby to call printHello 3 times
-myModule.callMeMoby(3, printHello);
