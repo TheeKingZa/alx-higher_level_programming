4c60fb473b721b72ee9bff600caaa6003c633b2c
Pule <proceedingmc@gmail.com>
2023-11-18 10:04:17 +0200

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -7,7 +7,7 @@ if (process.argv.length !== 4) {
 }
 
 // Define a function named 'add' that takes two parameters 'a' and 'b'.
- function add(a, b) {
+function add (a, b) {
   // Check if inputs are valid numbers
   if (isNaN(a) || isNaN(b)) {
     console.error('Invalid input. Please provide valid numbers.');
