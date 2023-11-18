3ab06370d50370021492092ed7277814bb2c707b
Pule <proceedingmc@gmail.com>
2023-11-18 15:16:29 -0800

    model_city.py

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module that defines the City class, which is a SQLAlchemy model
+representing the 'cities' table in the database.
+"""
+
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+class City(Base):
+    """
+    City class representing the 'cities' table in the database.
+
+    Attributes:
+        __tablename__ (str): The name of the database table.
+        id (int): Unique identifier for each city.
+        name (str): The name of the city.
+        state_id (int): Foreign key referencing the 'id' column in the 'states' table.
+    """
+
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
