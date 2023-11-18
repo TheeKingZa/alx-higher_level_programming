191ba066501bf9e95bea441e0d4c400f00979959
Pule <proceedingmc@gmail.com>
2023-11-18 10:01:19 +0200

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,24 @@
 #!/usr/bin/node
-// The above line is a shebang indicating that the script should be run using the specified interpreter, in this case, 'node'.
+
+// Check if the correct number of command-line arguments is provided
+if (process.argv.length !== 4) {
+  console.error('Usage: ./9-add.js <number1> <number2>');
+  process.exit(1); // Exit with a non-zero status to indicate an error
+}
 
 // Define a function named 'add' that takes two parameters 'a' and 'b'.
-function add (a, b) {
-  // Calculate the sum of 'a' and 'b' and store it in the variable 'c'.
-  const c = a + b;
+function add(a, b) {
+  // Check if inputs are valid numbers
+  if (isNaN(a) || isNaN(b)) {
+    console.error('Invalid input. Please provide valid numbers.');
+    process.exit(1);
+  }
+
+  // Calculate the sum of 'a' and 'b' and store it in the variable 'result'.
+  const result = a + b;
 
   // Print the sum to the console.
-  console.log(c);
+  console.log(result);
 }
 
 // Call the 'add' function, passing the numeric values of the third and fourth command-line arguments.
