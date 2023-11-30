31772229c4cf8e2887b46e603a40dc9b8f6a7af7
Pule <proceedingmc@gmail.com>
2023-11-30 04:21:23 -0800

    5-post-params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 # Bash script that sends a POST request to given URL.
-
-# Check if URL is provided as an argument
-if [ -z "$1" ]; then
-	    echo "Usage: $0 <URL>"
-	        exit 1
-fi
-
-# Set the variables for the POST request
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-# Use curl to send a POST request to the provided URL with the specified variables
-curl -s -X POST "$1" -d "email=$email&subject=$subject"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
 
