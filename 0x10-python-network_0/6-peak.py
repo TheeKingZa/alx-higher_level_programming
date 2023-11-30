07e718716e1cb907210cc1e365a1c0b22442cadc
Pule <proceedingmc@gmail.com>
2023-11-30 04:37:52 -0800

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
 
+
 def find_peak(list_of_integers):
-    """
-    Find a peak element in a given list using recursive binary search.
+        """
+        Find a peak element in a list of unsorted integers.
 
-    Args:
-    - list_of_integers: A list of integers
+        Args:
+            - list_of_integers: A list of integers
 
-    Returns:
-    - The peak element in the list
-    - None if the list is empty or None
-    """
+        Returns:
+            - A peak element in the list
+            - None if the list is empty
+        """
 
-    # Check if the input list is empty or None
-    if list_of_integers is None or list_of_integers == []:
-        return None
+        if not list_of_integers:
+            return None
 
-    # Initialize variables for binary search
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
+        lo, hi = 0, len(list_of_integers) - 1
 
-    # Base cases for lists of length 1 or 2
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
+        while lo < hi:
+            mid = (lo + hi) // 2
 
-    # Check if the middle element is a peak
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
+            if list_of_integers[mid] > list_of_integers[mid + 1]:
+                hi = mid
+            else:
+                lo = mid + 1
 
-    # Recursively search in the right half if the peak is on the right
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-
-    # Recursively search in the left half if the peak is on the left
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+        return list_of_integers[lo]
