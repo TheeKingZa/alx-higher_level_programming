742cec88ca2cdc321bddf8e6221255195293f8f4
Pule <proceedingmc@gmail.com>
2023-12-01 15:29:27 +0200

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
-import sys
-import requests
-
 """
 Display the X-Request-Id header
 variable of a request to a given
 URL
 """
+import sys
+import requests
+
 
 if __name__ == "__main__":
     # Get the URL from the command-line arguments
