07dee0053c5c456cac2f2f4f7b8d7ff1f5ae1df1
Pule <proceedingmc@gmail.com>
2024-01-03 13:00:26 +0200

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,36 +1,20 @@
 #!/usr/bin/node
+
+// Import the 'fs' module for file system operations
 const fs = require('fs');
 
-/**
- * Reads and prints the content of a file.
- * @param {string} filePath -The path of the file to read.
-  */
-function readFileContent(filePath) {
-	// Use fs.readFile to asynchronously read the file content
-	// in utf-8 encoding
-	fs.readFile(filePath, 'utf-8', (error, data) => {
-		if (error) {
-			// If an error occurs during reading,
-			// print the error object
-			console.error(error);
-		} else {
-			// Print the content of the file
-			// if reading is successful
-			console.log(data);
-		}
-	});
-}
+// Use fs.readFile to asynchronously read the file specified in the command line arguments
+// 'utf8' specifies the encoding of the file content to be read
+// The callback function is executed once the file is read or an error occurs
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  // The callback function receives two parameters: error and content
 
-// Check if the script is called with the correct number of arguments
-if (process.argv.length !== 3) {
-	// Print an error message if the correct number of arguments 
-	// is not provided
-	console.error('Usage: node 0-readme.js <file-path>');
-    } else {
-	    // Extract the file path from the command line arguments
-	    const filePath = process.argv[2];
-            
-	    // Call the function to read and print the content
-	    // of the specified file
-	    readFileContent(filePath);
-    }      
+  // If there is an error during file reading, print the error
+  // Otherwise, print the content of the file
+  console.log(error || content);
+});
+
+// Note: process.argv is an array that contains the command line arguments
+// process.argv[0] is the path to the Node.js executable
+// process.argv[1] is the path to the script being run
+// process.argv[2] is the first command line argument, which is expected to be the file path in this script
