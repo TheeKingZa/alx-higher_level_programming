c3c12b3ab225984e321d5ed2c2fbb7826623baf2
Pule <proceedingmc@gmail.com>
2024-01-03 13:18:43 +0200

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,38 +1,25 @@
 #!/usr/bin/node
 
+// Import the 'fs' module for file system operations
 const fs = require('fs');
 
-/**
- * Writes a string to a file.
- * @param {string} filePath - The path of the file to write.
- * @param {string} content - The string to write to the file.
- */
-function writeToFile(filePath, content) {
-    // Use fs.writeFile to asynchronously write content to the specified file path
-    // 'utf-8' specifies the encoding of the file content to be written
-    // The callback function is executed once the write operation is complete or an error occurs
-    fs.writeFile(filePath, content, 'utf-8', function (error) {
-        // The callback function receives one parameter: error
+// Use fs.writeFile to asynchronously write data to a file
+// The data to be written is specified by command line arguments
+// The first argument (process.argv[2]) is the file path
+// The second argument (process.argv[3]) is the content to be written
+// The third argument is a callback function executed once the write operation is complete or an error occurs
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  // The callback function receives one parameter: error
 
-        // If there is an error during writing, print the error
-        // Otherwise, indicate that the write operation was successful
-        if (error) {
-            console.error(error);
-        } else {
-            console.log('The file has been successfully written.');
-        }
-    });
-}
+  // Check if there is an error during the write operation
+  if (error) {
+    // If there is an error, log the error to the console
+    console.log(error);
+  }
+});
 
-// Check if the script is called with the correct number of arguments
-if (process.argv.length !== 4) {
-    // Print an error message if the correct number of arguments is not provided
-    console.error('Usage: node 1-writeme.js <file-path> <content>');
-} else {
-    // Extract the file path and content from the command line arguments
-    const filePath = process.argv[2];
-    const content = process.argv[3];
-
-    // Call the function to write the content to the specified file
-    writeToFile(filePath, content);
-}
+// Note: process.argv is an array that contains the command line arguments
+// process.argv[0] is the path to the Node.js executable
+// process.argv[1] is the path to the script being run
+// process.argv[2] is the first command line argument, which is expected to be the file path
+// process.argv[3] is the second command line argument, which is expected to be the content to be written to the file
