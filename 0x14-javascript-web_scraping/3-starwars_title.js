00bfae8ffe211d04250eddaa4b6fbfd5c5fe21c7
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2024-01-03 13:33:00 +0200

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,30 +1,40 @@
 #!/usr/bin/node
 
-// Import the 'request' module for making HTTP requests
 const request = require('request');
 
-// Retrieve the episode number from the command line arguments
-const episodeNum = process.argv[2];
+/**
+ * Prints the title of a Star Wars movie based on the given movie ID.
+ * @param {number} movieId - The ID of the Star Wars movie.
+ */
+function getStarWarsTitle(movieId) {
+    // Construct the URL using the provided endpoint and movie ID
+    const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-// Define the API URL for fetching Star Wars film information
-const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+    // Use the request module to make a GET request to the Star Wars API
+    request.get(apiUrl, function (error, response, body) {
+        // The callback function receives three parameters: error, response, and body
 
-// Make a GET request to the Star Wars API with the specified episode number
-request(API_URL + episodeNum, function (err, response, body) {
-  // The callback function receives three parameters: err, response, and body
+        if (error) {
+            // If there is an error during the request, print the error
+            console.error(error);
+        } else {
+            // Parse the JSON response body
+            const movieData = JSON.parse(body);
 
-  // Check if there is an error during the request
-  if (err) {
-    // If there is an error, log the error to the console
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    // If the response status code is 200 (OK), parse the JSON response body
-    const responseJSON = JSON.parse(body);
+            // Print the title of the Star Wars movie
+            console.log(movieData.title);
+        }
+    });
+}
 
-    // Print the title of the Star Wars film
-    console.log(responseJSON.title);
-  } else {
-    // If the response status code is not 200, log an error message with the status code
-    console.log('Error code: ' + response.statusCode);
-  }
-});
+// Check if the script is called with the correct number of arguments
+if (process.argv.length !== 3) {
+    // Print an error message if the correct number of arguments is not provided
+    console.error('Usage: node 3-starwars_title.js <movie-id>');
+} else {
+    // Extract the movie ID from the command line arguments
+    const movieId = process.argv[2];
+
+    // Call the function to get and print the title of the Star Wars movie
+    getStarWarsTitle(movieId);
+}
