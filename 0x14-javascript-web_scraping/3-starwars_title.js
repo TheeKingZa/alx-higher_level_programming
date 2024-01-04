b1c1e71acb465c10604c8bce2e18dbdaf7f4f300
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2024-01-04 09:44:39 +0200

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
-// Import the 'request' module for making HTTP requests
-const request = require('request');
+// This script retrieves information about a Star Wars movie based on its episode ID.
+// It uses the 'axios' library to make an HTTP request to the Star Wars API.
+// The API response is then parsed to extract and display the episode number and title of the movie.
+
+// Import the 'axios' library for making HTTP requests
+const axios = require('axios');
 
 // Retrieve the movie ID from the command line arguments
 const movieId = process.argv[2];
@@ -9,25 +13,17 @@ const movieId = process.argv[2];
 // Construct the API URL using the provided movie ID
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-// Make an HTTP request to the Star Wars API
-request(apiUrl, (error, response, body) => {
-  // Handle any errors that may occur during the request
-  if (error) {
-    console.error(error);
-    // Exit the script with a non-zero status code to indicate an error
-    process.exit(1);
-  }
-
-  // Check if the response status code is 200 (OK)
-  if (response.statusCode === 200) {
-    // Parse the JSON response body into an object
-    const movie = JSON.parse(body);
+// Make an HTTP GET request to the Star Wars API using 'axios'
+axios.get(apiUrl)
+  .then(response => {
+    // Extract the movie data from the response
+    const movie = response.data;
     // Display the episode number and title of the movie
     console.log(`Episode ${movie.episode_id}: ${movie.title}`);
-  } else {
-    // Display an error message if the response status code is not 200
-    console.error(`Error: ${response.statusCode}`);
+  })
+  .catch(error => {
+    // Handle errors during the HTTP request
+    console.error(error.message);
     // Exit the script with a non-zero status code to indicate an error
     process.exit(1);
-  }
-});
+  });
