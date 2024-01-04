9cec926a7bddf69fafeec643b520efe053e50684
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2024-01-04 09:35:46 +0200

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,28 +3,31 @@
 // Import the 'request' module for making HTTP requests
 const request = require('request');
 
-// Retrieve the episode number from the command line arguments
-const episodeNum = process.argv[2];
+// Retrieve the movie ID from the command line arguments
+const movieId = process.argv[2];
 
-// Define the API URL for fetching Star Wars film information
-const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+// Construct the API URL using the provided movie ID
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-// Make a GET request to the Star Wars API with the specified episode number
-request(API_URL + episodeNum, function (err, response, body) {
-  // The callback function receives three parameters: err, response, and body
+// Make an HTTP request to the Star Wars API
+request(apiUrl, (error, response, body) => {
+  // Handle any errors that may occur during the request
+  if (error) {
+    console.error(error);
+    // Exit the script with a non-zero status code to indicate an error
+    process.exit(1);
+  }
 
-  // Check if there is an error during the request
-  if (err) {
-    // If there is an error, log the error to the console
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    // If the response status code is 200 (OK), parse the JSON response body
-    const responseJSON = JSON.parse(body);
-
-    // Print the title of the Star Wars film
-    console.log(responseJSON.title);
+  // Check if the response status code is 200 (OK)
+  if (response.statusCode === 200) {
+    // Parse the JSON response body into an object
+    const movie = JSON.parse(body);
+    // Display the episode number and title of the movie
+    console.log(`Episode ${movie.episode_id}: ${movie.title}`);
   } else {
-    // If the response status code is not 200, log an error message with the status code
-    console.log('Error code: ' + response.statusCode);
+    // Display an error message if the response status code is not 200
+    console.error(`Error: ${response.statusCode}`);
+    // Exit the script with a non-zero status code to indicate an error
+    process.exit(1);
   }
 });
